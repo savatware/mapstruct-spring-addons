1651723e22341a743703ff77d5864bb2ce3db77b
Pieter-Jan Savat <info@villaratio.com>
2025-10-25 23:12:15 +0200

    wip

--- src/test/java/io/github/savatware/mapstruct/extensions/spring/processor/MappingMetadataProcessorTest.java
+++ src/test/java/io/github/savatware/mapstruct/extensions/spring/processor/MappingMetadataProcessorTest.java
@@ -11,15 +11,13 @@ import java.util.List;
 import static com.google.testing.compile.Compiler.javac;
 import static io.github.savatware.mapstruct.extensions.spring.testutils.CompilationAssert.assertThat;
 
-public class MappingMetadataProcessorTest {
+class MappingMetadataProcessorTest {
 
     @Test
     void whenClassNameSpecified_thenFileName_isCorrect() {
         // given
+        var metadataProcessor = createMappingMetadataProcessor();
         var mapperJavaFile = TestResourceReader.readJavaFileObject("MyMapper");
-        var metadataProcessor = new MappingMetadataProcessor();
-        var now = LocalDateTime.of(2025, 10, 16, 9, 32, 43, 123).atZone(ZoneId.of("Europe/Brussels"));
-        metadataProcessor.setDateTime(now);
         var expectedSourceCode = TestResourceReader.readJavaSource("MyMappingMetadata");
 
         // when
@@ -30,6 +28,13 @@ public class MappingMetadataProcessorTest {
         // then
         assertThat(compilation).succeeded();
         assertThat(compilation).generatedSourceCode("com.mycompany.test.MyMappingMetadata").isEqualTo(expectedSourceCode);
+    }
+
+    private MappingMetadataProcessor createMappingMetadataProcessor() {
+        var metadataProcessor = new MappingMetadataProcessor();
+        var now = LocalDateTime.of(2025, 10, 16, 9, 32, 43, 123).atZone(ZoneId.of("Europe/Brussels"));
+        metadataProcessor.setDateTime(now);
+        return metadataProcessor;
     }
 
 }
