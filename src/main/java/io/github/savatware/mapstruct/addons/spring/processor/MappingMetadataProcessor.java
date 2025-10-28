e64cfde2d7af4968185448285e6f586bd9c397a4
Pieter-Jan Savat <info@villaratio.com>
2025-10-28 09:05:57 +0100

    wip

--- src/main/java/io/github/savatware/mapstruct/addons/spring/processor/MappingMetadataProcessor.java
+++ src/main/java/io/github/savatware/mapstruct/addons/spring/processor/MappingMetadataProcessor.java
@@ -2,6 +2,7 @@ package io.github.savatware.mapstruct.addons.spring.processor;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import java.io.IOException;
@@ -10,6 +11,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static io.github.savatware.mapstruct.addons.spring.processor.GeneratedFileWriter.writeFile;
+import static javax.tools.Diagnostic.Kind.ERROR;
+import static javax.tools.Diagnostic.Kind.NOTE;
 
 /**
  * A JSR 269 annotation {@link Processor} which generates the implementations of metadata for mappings (methods
@@ -30,10 +33,6 @@ public class MappingMetadataProcessor extends AbstractProcessor {
         return SourceVersion.latestSupported();
     }
 
-    void setDateTime(ZonedDateTime dateTime) {
-        now = dateTime;
-    }
-
     public ZonedDateTime getDateTime() {
         if (now == null) {
             now = ZonedDateTime.now();
@@ -43,49 +42,61 @@ public class MappingMetadataProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        var generatedClasses = new HashSet<>();
+        var generatedClasses = new HashSet<String>();
         var elementInspector = new ElementInspector(processingEnv);
         for (var annotation : annotations) {
             for (var element : roundEnv.getElementsAnnotatedWith(annotation)) {
-                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "Processing mapping metadata for " + element.getSimpleName());
+                processingEnv.getMessager().printMessage(NOTE, "Processing mapping metadata for " + element.getSimpleName());
 
                 var attributes = new MappingAnnotationAttributes(elementInspector, element, getDateTime());
-
-                if (attributes.getClassName().contains(".")) {
-                    var containingClass = elementInspector.getContainingClass(element);
-                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
-                            "Class name can not contain a dot, received: " + attributes.getClassName() + ", defined in \"" + containingClass + "\"");
-                    continue; // skip invalid class names
+                if (hasInvalidAttributes(attributes, elementInspector, element)) {
+                    continue;
                 }
-
-                if (attributes.getClassName().isEmpty()) {
-                    var containingClass = elementInspector.getContainingClass(element);
-                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
-                            "Not allowed to define an empty classname, defined in \"" + containingClass + "\"");
-                    continue; // skip invalid class names
-                }
-
-                try {
-                    var fqdn = attributes.getFullyQualifiedName();
-                    if (generatedClasses.contains(fqdn)) {
-                        processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
-                                "Found duplicate class \"" + fqdn + "\"");
-                        continue;
-                    }
-                    generatedClasses.add(fqdn);
-
-                    var filer = processingEnv.getFiler();
-                    var fileObject = filer.createSourceFile(fqdn);
-                    try (var writer = fileObject.openWriter()) {
-                        writeFile(writer, attributes);
-                    }
-                } catch (IOException e) {
-                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Failed to generate class: " + e.getMessage());
-                }
+                generate(attributes, generatedClasses);
             }
         }
         return true; // No further processing of this annotation
     }
 
+    void setDateTime(ZonedDateTime dateTime) {
+        now = dateTime;
+    }
+
+    private boolean hasInvalidAttributes(MappingAnnotationAttributes attributes, ElementInspector elementInspector, Element element) {
+        if (attributes.getClassName().contains(".")) {
+            var containingClass = elementInspector.getContainingClass(element);
+            processingEnv.getMessager().printMessage(ERROR,
+                    "Class name can not contain a dot, received: " + attributes.getClassName() + ", defined in \"" + containingClass + "\"");
+            return true;
+        }
+
+        if (attributes.getClassName().isEmpty()) {
+            var containingClass = elementInspector.getContainingClass(element);
+            processingEnv.getMessager().printMessage(ERROR,
+                    "Not allowed to define an empty classname, defined in \"" + containingClass + "\"");
+            return true;
+        }
+
+        return false;
+    }
+
+    private void generate(MappingAnnotationAttributes attributes, Set<String> generatedClasses) {
+        try {
+            var fqdn = attributes.getFullyQualifiedName();
+            if (generatedClasses.contains(fqdn)) {
+                processingEnv.getMessager().printMessage(ERROR, "Found duplicate class \"" + fqdn + "\"");
+                return;
+            }
+            generatedClasses.add(fqdn);
+
+            var filer = processingEnv.getFiler();
+            var fileObject = filer.createSourceFile(fqdn);
+            try (var writer = fileObject.openWriter()) {
+                writeFile(writer, attributes);
+            }
+        } catch (IOException e) {
+            processingEnv.getMessager().printMessage(ERROR, "Failed to generate class: " + e.getMessage());
+        }
+    }
 }
 
